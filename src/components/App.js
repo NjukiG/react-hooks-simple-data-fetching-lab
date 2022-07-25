595934ccda5f6e1c491f97f7d33a9f12761a9e62
George <gnjuki19@gmail.com>
2022-07-25 09:02:38 +0300

    Practiced using the useEffect hook to fetch data and render images on the DOM

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App(){
+    const [aStateImage, setAStateImage] = useState(null);
+
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(responce => responce.json())
+            .then(data => {
+                setAStateImage(data.message);
+            })
+    }, [])
+
+
+    return (
+        <div>
+            {(!aStateImage ? <p>Loading...</p> : <img src={aStateImage} alt="A Random Dog" />)}
+        </div>
+    )
+}
+
+export default App;
+
